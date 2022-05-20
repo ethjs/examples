1b63fd1b532c60fe869c4f473109b71ba2fce820
fhildeb <felix@lukso.io>
2022-05-20 10:29:41 +0200

    utils: add checkMinimalBalance function

--- utils.js
+++ utils.js
@@ -43,4 +43,19 @@ async function connectWeb3() {
       el("#browser").style.display = "block";
       return false;
     }
-  }
+}
+
+async function checkMinimalBalance() {
+    // Access web3
+    const web3 = new Web3(window.ethereum);
+
+    // Get account
+    let accounts = await ethereum.request({ method: "eth_accounts" });
+
+    // Get the account balance and check if it is above 0.25 LYXt
+    if(web3.utils.fromWei(await web3.eth.getBalance(accounts[0]), 'ether') < 0.25){
+
+        // Show low balance browser notification
+      el("#lowBalance").style.display = "block";
+    }
+}
