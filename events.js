2d5671648d43d548fd7fcfdc8fd296556365a753
fhildeb <felix@lukso.io>
2022-05-20 10:46:54 +0200

    add events.js

---
+++ events.js
@@ -0,0 +1,29 @@
+// Helper function to search for HTML elements
+let el = function (id) { return document.querySelector(id); };
+
+// Before adding events to HTML elements, load notifications into the Webpage
+$('#notifications').load('notifications.html', function() {
+
+  // If Web3 extension found
+  if (window.ethereum) {
+
+    // Enable login button functionality
+    el("#loginbutton").addEventListener("click", async function () {
+    
+      // Request login of extension
+      await ethereum.request({ method: "eth_requestAccounts" });
+    
+      // Hide button after login was successful
+      el("#login").style.display = "none";
+    
+      // Restart application with logged-in extension
+      app();
+    });
+
+    // Enable network swap
+      el("#swapnetwork").addEventListener("click", async function () {
+      await addLuksoTestnet();
+      el("#network").style.display = "none";
+    });
+  }
+});
