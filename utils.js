6f13e47dc809d17e8c6d35c3e43329a960518cac
fhildeb <felix@lukso.io>
2022-06-29 21:02:37 +0200

    styling

--- utils.js
+++ utils.js
@@ -1,50 +1,43 @@
 async function connectWeb3() {
+  // Check if Browser is Chrome or Firefox
+  if (
+    navigator.userAgent.indexOf("Firefox") != -1 ||
+    navigator.userAgent.indexOf("Chrome") != -1
+  ) {
+    // Check if browser extension is installed
 
-    // Check if Browser is Chrome or Firefox
-    if (
-      navigator.userAgent.indexOf("Firefox") != -1 ||
-      navigator.userAgent.indexOf("Chrome") != -1
-    ) {
-      
-      // Check if browser extension is installed
-  
-      // Get account
-      if (window.ethereum) {
-  
-        // Request account
-        let account = await ethereum.request({ method: "eth_accounts" });
-  
-        // If no account was found
-        if (!account.length) {
-  
-          // Show login button 
-          el("#login").style.display = "block";
-          return false;
-        } 
-        else {
-          return true;
-        }
+    // Get account
+    if (window.ethereum) {
+      // Request account
+      let account = await ethereum.request({ method: "eth_accounts" });
+
+      // If no account was found
+      if (!account.length) {
+        // Show login button
+        el("#login").style.display = "block";
+        return false;
+      } else {
+        return true;
       }
-      
-      // Provider not set or invalid
-      else{
+    }
 
+    // Provider not set or invalid
+    else {
       // Show install extension notification
       el("#install").style.display = "block";
       el("#singular").style.display = "none";
       return false;
-      }
-    }
-    
-    // Browser is not supported
-    else {
-  
-      // Show unsupported browser notification
-      el("#browser").style.display = "block";
-      el("#singular").style.display = "none";
-      return false;
     }
   }
+
+  // Browser is not supported
+  else {
+    // Show unsupported browser notification
+    el("#browser").style.display = "block";
+    el("#singular").style.display = "none";
+    return false;
+  }
+}
 
 async function checkMinimalBalance() {
   // Access web3
